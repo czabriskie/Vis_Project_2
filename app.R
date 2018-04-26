9fb85d0e0ca9acfd74fed98ae9974d970972d7a3
ericmckinney77 <ericmckinney77@gmail.com>
2018-04-26 14:57:04 -0600

    Fixed colors

--- app.R
+++ app.R
@@ -191,8 +191,8 @@ server <- function(input, output) {
         #             fill = brewer.pal(5, 'Set2')[1], alpha = 0.7) +
         # geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF), data = state.data, alpha = 0.7,
         #             fill = brewer.pal(5, 'Set2')[2]) +
-        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         geom_line(data = state.data, aes(colour = '#998ec3'), size = 1.2) +
+        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         scale_color_manual(values = c('#f1a340', '#998ec3'), labels = c('City', 'State'), name = '') +
         scale_y_continuous(limits = c(5 * floor(min(city.data$Mean_TemperatureF) / 5), 
                                       5 * ceiling(max(city.data$Mean_TemperatureF) / 5)),
@@ -215,8 +215,8 @@ server <- function(input, output) {
         #             fill = brewer.pal(5, 'Set2')[2], alpha = 0.7) +
         # geom_ribbon(aes(ymin = Min_Humidity, ymax = Max_Humidity),
         #             fill = brewer.pal(5, 'Set2')[3], alpha = 0.7, data = state.data) +
-        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         geom_line(data = state.data, aes(colour = '#998ec3'), size = 1.2) +
+        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         scale_color_manual(values = c('#f1a340', '#998ec3'), labels = c('City', 'State'), name = '') +
         scale_y_continuous(limits = c(5 * floor(min(city.data$Mean_Humidity) / 5), 
                                       5 * ceiling(max(city.data$Mean_Humidity) / 5)),
@@ -235,8 +235,8 @@ server <- function(input, output) {
     }
     else if(input$feature == 'wind.speed' ){
         plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = Mean_Wind_SpeedMPH)) +
-          geom_line(aes(colour = '#f1a340'), size = 1.2) +
           geom_line(data = state.data, aes(colour = '#998ec3'), size = 1.2) +
+          geom_line(aes(colour = '#f1a340'), size = 1.2) +
           scale_color_manual(values = c('#f1a340', '#998ec3'), labels = c('City', 'State'), name = '') +
           scale_y_continuous(limits = c(5 * floor(min(city.data$Mean_Wind_SpeedMPH) / 5), 
                                         5 * ceiling(max(city.data$Mean_Wind_SpeedMPH) / 5)),
@@ -255,8 +255,8 @@ server <- function(input, output) {
     }
     else if(input$feature == 'precip'){
       plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = PrecipitationIn)) +
-        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         geom_line(data = state.data, aes(colour = '#998ec3'), size = 1.2) +
+        geom_line(aes(colour = '#f1a340'), size = 1.2) +
         scale_color_manual(values = c('#f1a340', '#998ec3'), labels = c('City', 'State'), name = '') +
         scale_y_continuous(limits = c(floor(min(city.data$PrecipitationIn)), 
                                       ceiling(max(city.data$PrecipitationIn))),
