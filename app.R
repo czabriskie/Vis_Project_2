47e26b239ed4665527f5e52d872eced2945c2f3a
czabriskie <31149669+czabriskie@users.noreply.github.com>
2018-04-25 18:08:04 -0600

    Added Title to plots

--- app.R
+++ app.R
@@ -9,7 +9,8 @@ library(ggplot2)
 library(ggmap)
 library(RColorBrewer)
 library(gridExtra)
- 
+library(grid)
+
 theme_update(plot.title = element_text(hjust = 0.5))
 
 # read in data
@@ -24,31 +25,31 @@ cities.states <- weather %>% select(city, state, longitude, latitude) %>% distin
 
 # Content of Page
 ui <- fluidPage(theme = shinytheme("superhero"),
-  titlePanel('2018 Data Expo Shiny App'),
-  br(),
-  sidebarLayout(
-    mainPanel(
-      leafletOutput('map', height='400px'),
-      absolutePanel(top = -10, left = 70, textInput('target_zone', '' , 'Ex: Salt Lake City'))
-      ),
-    sidebarPanel(
-      radioButtons("feature", h3("Data to Display"),
-                         c("Temperature" = "temp",
-                           "Humidity" = "humid",
-                           "Wind Speed" = "wind.speed",
-                           "Precipitation" = "precip"),
-                         selected = "temp"),
-      br(),
-      sliderInput("dateslider",
-                  label = h3("Date Range"),
-                  min = as.Date("2014-07-01"),
-                  max = as.Date("2017-09-01"),
-                  value = as.Date(c("2015-01-01", "2015-06-01"))),
-      br()
-      ), position = "left"),
-  plotOutput('plot'),
-  HTML('<p>Eric McKiney and Cameron Zabriskie</p>')
-  )
+                titlePanel('2018 Data Expo Shiny App'),
+                br(),
+                sidebarLayout(
+                  mainPanel(
+                    leafletOutput('map', height='400px'),
+                    absolutePanel(top = -10, left = 70, textInput('target_zone', '' , 'Ex: Salt Lake City'))
+                  ),
+                  sidebarPanel(
+                    radioButtons("feature", h3("Data to Display"),
+                                 c("Temperature" = "temp",
+                                   "Humidity" = "humid",
+                                   "Wind Speed" = "wind.speed",
+                                   "Precipitation" = "precip"),
+                                 selected = "temp"),
+                    br(),
+                    sliderInput("dateslider",
+                                label = h3("Date Range"),
+                                min = as.Date("2014-07-01"),
+                                max = as.Date("2017-09-01"),
+                                value = as.Date(c("2015-01-01", "2015-06-01"))),
+                    br()
+                  ), position = "left"),
+                plotOutput('plot'),
+                HTML('<p>Eric McKiney and Cameron Zabriskie</p>')
+)
 
 # Server Information
 server <- function(input, output) {
@@ -102,7 +103,7 @@ server <- function(input, output) {
     state.data <- weather %>% filter( state == gsub(', ', '', regmatches(place, regexpr(',.+', place))),
                                       Date >= as.Date(input$dateslider[[1]]) & Date <= as.Date(input$dateslider[[2]])) %>% 
       select(-state) %>% group_by(Date) %>% select(-c(city, Events, AirPtCd)) %>% summarise_all(mean, na.rm = TRUE) 
-
+    
     plots <- list(NULL, NULL, NULL, NULL)
     inputs <- c()
     
@@ -111,23 +112,19 @@ server <- function(input, output) {
         plots[[i]] <- ggplot(data = city.data, aes(x = Date, y = Mean_TemperatureF)) +
           geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF),
                       fill = brewer.pal(5, "Set2")[1], alpha = 0.7) +
-          geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF), data = state.data, alpha = 0.7,
-                      fill = brewer.pal(5, "Set2")[2]) +
           geom_line() +
-          geom_line(data = state.data, col = 'red') +
           scale_x_date(limits = as.Date(c(input$dateslider))) +
           labs(title = "Max, Mean, and Min Temperatures",
                x = "",
                y = "Temperature (in Fahrenheit)")
         
-          inputs <- c(inputs, 'temp')
+        inputs <- c(inputs, 'temp')
       }
       if(input$feature != 'humid'  && is.null(plots[[i]]) && !('humid' %in% inputs)){
         plots[[i]]  <- ggplot(data = city.data, aes(x = Date, y = Mean_Humidity)) +
           geom_ribbon(aes(ymin = Min_Humidity, ymax = Max_Humidity),
                       fill = brewer.pal(5, "Set2")[3], alpha = 0.7) +
           geom_line() +
-          geom_line(data = state.data, col = 'red') +
           scale_x_date(limits = as.Date(c(input$dateslider))) +
           labs(title = "Max, Mean, and Min Humidity",
                x = "",
@@ -138,9 +135,8 @@ server <- function(input, output) {
       if(input$feature != 'wind.speed' && is.null(plots[[i]]) && !('wind.speed' %in% inputs)){
         plots[[i]]  <- ggplot(data = city.data, aes(x = Date, y = Mean_Wind_SpeedMPH)) +
           geom_line() +
-          geom_line(data = state.data, col = 'red') +
           scale_x_date(limits = as.Date(c(input$dateslider))) +
-          labs(title = "Max, Mean, and Min Wind Speed",
+          labs(title = "Wind Speed",
                x = "",
                y = "Wind Speed (in MPH)")
         
@@ -149,7 +145,6 @@ server <- function(input, output) {
       if(input$feature != 'precip'  && is.null(plots[[i]]) && !('precip' %in% inputs)){
         plots[[i]]  <- ggplot(data = city.data, aes(x = Date, y = PrecipitationIn)) +
           geom_line() +
-          geom_line(data = state.data, col = 'red') +
           scale_x_date(limits = as.Date(c(input$dateslider))) +
           labs(title = "Daily Precipitation",
                x = "",
@@ -162,36 +157,38 @@ server <- function(input, output) {
     
     if (input$feature == 'temp'){
       plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = Mean_TemperatureF)) +
-                     geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF),
-                                 fill = brewer.pal(5, "Set2")[1], alpha = 0.7) +
-                     geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF), data = state.data, alpha = 0.7,
-                                 fill = brewer.pal(5, "Set2")[2]) +
-                     geom_line() +
-                     geom_line(data = state.data, col = 'red') +
-                     scale_x_date(limits = as.Date(c(input$dateslider))) +
-                     labs(title = "Max, Mean, and Min Temperatures",
-                          x = "",
-                          y = "Temperature (in Fahrenheit)")
+        geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF),
+                    fill = brewer.pal(5, "Set2")[1], alpha = 0.7) +
+        geom_ribbon(aes(ymin = Min_TemperatureF, ymax = Max_TemperatureF), data = state.data, alpha = 0.7,
+                    fill = brewer.pal(5, "Set2")[2]) +
+        geom_line() +
+        geom_line(data = state.data, col = 'red') +
+        scale_x_date(limits = as.Date(c(input$dateslider))) +
+        labs(title = "Max, Mean, and Min Temperatures",
+             x = "",
+             y = "Temperature (in Fahrenheit)") 
     }
     else if(input$feature == 'humid'){
       plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = Mean_Humidity)) +
-                     geom_ribbon(aes(ymin = Min_Humidity, ymax = Max_Humidity),
-                                 fill = brewer.pal(5, "Set2")[3], alpha = 0.7) +
-                     geom_line() +
-                     geom_line(data = state.data, col = 'red') +
-                     scale_x_date(limits = as.Date(c(input$dateslider))) +
-                     labs(title = "Max, Mean, and Min Humidity",
-                          x = "",
-                          y = "Humidity (as a Percentage)")
+        geom_ribbon(aes(ymin = Min_Humidity, ymax = Max_Humidity),
+                    fill = brewer.pal(5, "Set2")[3], alpha = 0.7) +
+        geom_ribbon(aes(ymin = Min_Humidity, ymax = Max_Humidity),
+                    fill = brewer.pal(5, "Set2")[3], alpha = 0.7, data = state.data) +
+        geom_line(aes(colour = 'blue')) +
+        geom_line(data = state.data, aes(colour = 'red')) +
+        scale_x_date(limits = as.Date(c(input$dateslider))) +
+        labs(title = "Max, Mean, and Min Humidity",
+             x = "",
+             y = "Humidity (as a Percentage)")
     }
     else if(input$feature == 'wind.speed' ){
       plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = Mean_Wind_SpeedMPH)) +
-                     geom_line() +
-                     geom_line(data = state.data, col = 'red') +
-                     scale_x_date(limits = as.Date(c(input$dateslider))) +
-                     labs(title = "Max, Mean, and Min Wind Speed",
-                          x = "",
-                          y = "Wind Speed (in MPH)")
+        geom_line() +
+        geom_line(data = state.data, col = 'red') +
+        scale_x_date(limits = as.Date(c(input$dateslider))) +
+        labs(title = "Wind Speed",
+             x = "",
+             y = "Wind Speed (in MPH)")
     }
     else if(input$feature == 'precip'){
       plots[[4]] <- ggplot(data = city.data, aes(x = Date, y = PrecipitationIn)) +
@@ -203,13 +200,16 @@ server <- function(input, output) {
              y = "Precipitation (in Inches)")
     }
     
+    
+    
     lay <- rbind(c(4, 4, 4, 1, 2),
                  c(4, 4, 4, 3, 3))
-
-    grid.arrange(grobs = plots, layout_matrix = lay)
-
-
-    })
+    
+    # https://stackoverflow.com/questions/14726078/changing-title-in-multiplot-ggplot2-using-grid-arrange
+    grid.arrange(grobs = plots, layout_matrix = lay, top = textGrob(place,gp=gpar(fontsize=20,font=3)))
+    
+    
+  })
 }
 
 shinyApp(ui = ui, server = server)
